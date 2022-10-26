9a156d990fdd8d735c0bb8f3816a8964a32cb671
jong6598 <108744804+jong6598@users.noreply.github.com>
2022-10-27 05:07:20 +0900

    🎨 추가 로그인 회원가입 페이지 라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Auth from './pages/Auth';
+
 function App() {
-	return <div className="App">Hello ~!</div>;
+	return (
+		<div className="App">
+			<Router>
+				<Routes>
+					<Route path="/" element={<Auth />} />
+				</Routes>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
