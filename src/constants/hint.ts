1d1d0d5fa5f0e06d952754e25a4867417ca9ce74
jong6598 <108744804+jong6598@users.noreply.github.com>
2022-10-28 00:30:07 +0900

    🔨유효성 검사를 위한 커스텀 훅 생성
    Co-authored-by: 박상호 <hopak-e@users.noreply.github.com>
    Co-authored-by: OD <kg-te@naver.com>

--- src/constants/hint.ts
+++ src/constants/hint.ts
@@ -4,8 +4,6 @@ interface Hints {
 	passwordConfirm: string;
 }
 
-// export const HINT_TYPE = 'email' | 'password' | 'passwordConfirm';
-
 export const HINT_LIST: Hints = {
 	email: '@를 꼭 입력해주세요.',
 	password: '8자리 이상 입력해주세요.',
