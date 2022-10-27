773fcc999cfa1a29ccb89c47963984ad35c921c5
jong6598 <108744804+jong6598@users.noreply.github.com>
2022-10-27 19:48:36 +0900

    🎨 변경 baseURL 환변수화

--- src/apis/requester.ts
+++ src/apis/requester.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-	baseURL: `https://pre-onboarding-selection-task.shop`,
+	baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use((config) => {
