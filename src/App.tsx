f2ebe9ebb298a1751c9f6890dcf3cb869701a196
jong6598 <108744804+jong6598@users.noreply.github.com>
2022-10-27 16:09:31 +0900

    🎨 Todo 페이지 라우팅 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
 import Auth from './pages/Auth';
+import Todos from './pages/Todos';
 
 function App() {
+	const isLogged: boolean = Boolean(localStorage.getItem('accessToken'));
 	return (
 		<div className="App">
 			<Router>
 				<Routes>
-					<Route path="/" element={<Auth />} />
+					<Route path="/" element={isLogged ? <Navigate to="/todo" /> : <Auth />} />
+					<Route path="/todo" element={<Todos />} />
 				</Routes>
 			</Router>
 		</div>
