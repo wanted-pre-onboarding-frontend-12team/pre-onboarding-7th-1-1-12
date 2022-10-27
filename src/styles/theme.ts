21d5f70b079ef5d0f9040dc7b26ebc26ebde2272
OD <kg-te@naver.com>
2022-10-27 19:58:51 +0900

    :lipstick: 테마 추가

---
+++ src/styles/theme.ts
@@ -0,0 +1,33 @@
+const fontSize = {
+	small: '16px',
+	medium: '18px',
+	large: '20px',
+};
+
+const fontWeight = {
+	light: 300,
+	normal: 400,
+	medium: 500,
+	semiBold: 600,
+	bold: 700,
+	strongBold: 800,
+};
+
+const colors = {
+	primary: '#273c75',
+	success: '#44bd32',
+	warning: '#fbc531',
+	error: '#e74c3c',
+	info: '#3498db',
+	white: '#f1f2f6',
+	black: '#333333',
+};
+
+const Theme = {
+	margin: '16px',
+	fontSize,
+	fontWeight,
+	colors,
+};
+
+export default Theme;
