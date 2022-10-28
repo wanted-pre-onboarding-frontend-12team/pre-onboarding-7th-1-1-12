a1e30a86a0705b7ab3317117cd123859e60c0a7c
youngmin wi <youngminieo1005@gmail.com>
2022-10-28 23:34:14 +0900

    :sparkles: 투두 앱 페이지 구현

--- src/pages/Todo/index.tsx
+++ src/pages/Todo/index.tsx
@@ -1,5 +1,31 @@
-const Todo = () => {
-	return <div>Todo Page</div>;
+import * as S from './styled';
+import Layout from '../../components/layout';
+import { TodoInputForm, TodoItem } from '../../components';
+import useTodos from './useTodos';
+
+const TodoApp = () => {
+	const { todos, onCreateNewTodoSubmit, onTodoCompletedToggle, onTodoItemEdit, onTodoItemDelete } = useTodos();
+
+	return (
+		<Layout>
+			<S.Container>
+				<TodoInputForm onCreateNewTodoSubmit={onCreateNewTodoSubmit} />
+				{todos.length >= 0 && (
+					<S.TodoList>
+						{todos.map((todo) => (
+							<TodoItem
+								key={todo.id}
+								todo={todo}
+								onTodoCompletedToggle={onTodoCompletedToggle}
+								onTodoItemEdit={onTodoItemEdit}
+								onTodoItemDelete={onTodoItemDelete}
+							/>
+						))}
+					</S.TodoList>
+				)}
+			</S.Container>
+		</Layout>
+	);
 };
 
-export default Todo;
+export default TodoApp;
