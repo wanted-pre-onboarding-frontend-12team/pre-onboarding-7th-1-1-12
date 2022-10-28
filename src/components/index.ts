eca92859922a25089481697b22978041a6a75de9
youngmin wi <youngminieo1005@gmail.com>
2022-10-28 21:59:55 +0900

    :sparkles: assignment 1,2,3 - 로그인/회원가입
    + 이메일 & 비밀번호 유효성 검사 기능
    + 로그인, 회원가입 기능
    + 로그인 여부에 따른 리다이렉팅 처리

---
+++ src/components/index.ts
@@ -0,0 +1,14 @@
+// Layout Component
+export { default as Layout } from './layout';
+
+// Shared Components
+export { default as Input } from './shared/Input';
+export { default as Button } from './shared/Button';
+
+// Feature Components
+export { default as AuthForm } from './feature/AuthForm';
+export { default as TodoInputForm } from './feature/TodoInputForm';
+export { default as TodoItem } from './feature/TodoItem';
+
+// Only Styled Component
+export { default as FlexBox } from './layout/styled';
---
+++ src/components/layout/index.tsx
@@ -0,0 +1,7 @@
+import { PropsWithChildren } from 'react';
+
+const Layout = (props: PropsWithChildren) => {
+	return <>{props.children}</>;
+};
+
+export default Layout;
